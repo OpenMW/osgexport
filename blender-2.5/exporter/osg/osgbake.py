548a0b4b3eeb28f3c31ad92c77b4753daf9bba84
Aurélien Chatelain <aurelien.chatelain@utbm.fr>
2015-10-08 10:52:52 +0200

    [#104902938] Imports bake function from Blender module (bpy_extras.anim_utils)
    The "bake_action" function has been imported in osgexport code to make improvements on
    the baking process easier (add options...)

--- blender-2.5/exporter/osg/osgbake.py
+++ blender-2.5/exporter/osg/osgbake.py
@@ -17,12 +17,221 @@
 # ##### END GPL LICENSE BLOCK #####
 
 # <pep8-80 compliant>
-from bpy_extras.anim_utils import bake_action
+import bpy
+
+
+# This function comes from bpy_extras.anim_utils and has been
+# added here to allow more control on baking process
+def bakeAction(frame_start,
+               frame_end,
+               frame_step=1,
+               only_selected=False,
+               do_pose=True,
+               do_object=True,
+               do_visual_keying=True,
+               do_constraint_clear=False,
+               do_parents_clear=False,
+               do_clean=False,
+               action=None,
+               ):
+
+    """
+    Return an image from the file path with options to search multiple paths
+    and return a placeholder if its not found.
+
+    :arg frame_start: First frame to bake.
+    :type frame_start: int
+    :arg frame_end: Last frame to bake.
+    :type frame_end: int
+    :arg frame_step: Frame step.
+    :type frame_step: int
+    :arg only_selected: Only bake selected data.
+    :type only_selected: bool
+    :arg do_pose: Bake pose channels.
+    :type do_pose: bool
+    :arg do_object: Bake objects.
+    :type do_object: bool
+    :arg do_visual_keying: Use the final transformations for baking ('visual keying')
+    :type do_visual_keying: bool
+    :arg do_constraint_clear: Remove constraints after baking.
+    :type do_constraint_clear: bool
+    :arg do_parents_clear: Unparent after baking objects.
+    :type do_parents_clear: bool
+    :arg do_clean: Remove redundant keyframes after baking.
+    :type do_clean: bool
+    :arg action: An action to bake the data into, or None for a new action
+       to be created.
+    :type action: :class:`bpy.types.Action` or None
+
+    :return: an action or None
+    :rtype: :class:`bpy.types.Action`
+    """
+
+    # -------------------------------------------------------------------------
+    # Helper Functions and vars
+
+    def poseFrameInfo(obj, do_visual_keying):
+        matrix = {}
+        for name, pbone in obj.pose.bones.items():
+            if do_visual_keying:
+                # Get the final transform of the bone in its own local space...
+                matrix[name] = obj.convert_space(pbone, pbone.matrix, 'POSE', 'LOCAL')
+            else:
+                matrix[name] = pbone.matrix_basis.copy()
+        return matrix
+
+    if do_parents_clear:
+        def objFrameInfo(obj, do_visual_keying):
+            parent = obj.parent
+            matrix = obj.matrix_local if do_visual_keying else obj.matrix_local
+            if parent:
+                return parent.matrix_world * matrix
+            else:
+                return matrix.copy()
+    else:
+        def objFrameInfo(obj, do_visual_keying):
+            return obj.matrix_local.copy() if do_visual_keying else obj.matrix_basis.copy()
+
+    # -------------------------------------------------------------------------
+    # Setup the Context
+
+    # TODO, pass data rather then grabbing from the context!
+    scene = bpy.context.scene
+    obj = bpy.context.object
+    frame_back = scene.frame_current
+
+    if obj.pose is None:
+        do_pose = False
+
+    if not (do_pose or do_object):
+        return None
+
+    pose_info = []
+    obj_info = []
+
+    options = {'INSERTKEY_NEEDED'}
+
+    frame_range = range(frame_start, frame_end + 1, frame_step)
+
+    # -------------------------------------------------------------------------
+    # Collect transformations
+
+    for f in frame_range:
+        scene.frame_set(f)
+        scene.update()
+        if do_pose:
+            pose_info.append(poseFrameInfo(obj, do_visual_keying))
+        if do_object:
+            obj_info.append(objFrameInfo(obj, do_visual_keying))
+
+    # -------------------------------------------------------------------------
+    # Create action
+
+    # in case animation data hasn't been created
+    atd = obj.animation_data_create()
+    if action is None:
+        action = bpy.data.actions.new("Action")
+    atd.action = action
+
+    # -------------------------------------------------------------------------
+    # Apply transformations to action
+
+    # pose
+    if do_pose:
+        for name, pbone in obj.pose.bones.items():
+            if only_selected and not pbone.bone.select:
+                continue
+
+            if do_constraint_clear:
+                while pbone.constraints:
+                    pbone.constraints.remove(pbone.constraints[0])
+
+            # create compatible eulers
+            euler_prev = None
+
+            for (f, matrix) in zip(frame_range, pose_info):
+                pbone.matrix_basis = matrix[name].copy()
+
+                pbone.keyframe_insert("location", -1, f, name, options)
+
+                rotation_mode = pbone.rotation_mode
+                if rotation_mode == 'QUATERNION':
+                    pbone.keyframe_insert("rotation_quaternion", -1, f, name, options)
+                elif rotation_mode == 'AXIS_ANGLE':
+                    pbone.keyframe_insert("rotation_axis_angle", -1, f, name, options)
+                else:  # euler, XYZ, ZXY etc
+                    if euler_prev is not None:
+                        euler = pbone.rotation_euler.copy()
+                        euler.make_compatible(euler_prev)
+                        pbone.rotation_euler = euler
+                        euler_prev = euler
+                        del euler
+                    else:
+                        euler_prev = pbone.rotation_euler.copy()
+                    pbone.keyframe_insert("rotation_euler", -1, f, name, options)
+
+                pbone.keyframe_insert("scale", -1, f, name, options)
+
+    # object. TODO. multiple objects
+    if do_object:
+        if do_constraint_clear:
+            while obj.constraints:
+                obj.constraints.remove(obj.constraints[0])
+
+        # create compatible eulers
+        euler_prev = None
+
+        for (f, matrix) in zip(frame_range, obj_info):
+            name = "Action Bake"  # XXX: placeholder
+            obj.matrix_basis = matrix
+
+            obj.keyframe_insert("location", -1, f, name, options)
+
+            rotation_mode = obj.rotation_mode
+            if rotation_mode == 'QUATERNION':
+                obj.keyframe_insert("rotation_quaternion", -1, f, name, options)
+            elif rotation_mode == 'AXIS_ANGLE':
+                obj.keyframe_insert("rotation_axis_angle", -1, f, name, options)
+            else:  # euler, XYZ, ZXY etc
+                if euler_prev is not None:
+                    euler = obj.rotation_euler.copy()
+                    euler.make_compatible(euler_prev)
+                    obj.rotation_euler = euler
+                    euler_prev = euler
+                    del euler
+                else:
+                    euler_prev = obj.rotation_euler.copy()
+                obj.keyframe_insert("rotation_euler", -1, f, name, options)
+
+            obj.keyframe_insert("scale", -1, f, name, options)
+
+        if do_parents_clear:
+            obj.parent = None
+
+    # -------------------------------------------------------------------------
+    # Clean
+
+    if do_clean:
+        for fcu in action.fcurves:
+            keyframe_points = fcu.keyframe_points
+            i = 1
+            while i < len(fcu.keyframe_points) - 1:
+                val_prev = keyframe_points[i - 1].co[1]
+                val_next = keyframe_points[i + 1].co[1]
+                val = keyframe_points[i].co[1]
+
+                if abs(val - val_prev) + abs(val - val_next) < 0.0001:
+                    keyframe_points.remove(keyframe_points[i])
+                else:
+                    i += 1
+
+    scene.frame_set(frame_back)
+
+    return action
 
 
 # take care of restoring selection after
 def bakeAnimation(scene, blender_object):
-    frame_range = (scene.frame_start, scene.frame_end, scene.frame_step)
     # baking will replace the current action but we want to keep scene unchanged
     if blender_object.animation_data and blender_object.animation_data.action:
         original_action = blender_object.animation_data.action
@@ -30,15 +239,16 @@ def bakeAnimation(scene, blender_object):
     # Baking is done on the active object
     active_object_backup = scene.objects.active
     scene.objects.active = blender_object
-    baked_action = bake_action(scene.frame_start,
-                               scene.frame_end,
-                               scene.frame_step,
-                               do_clean=True,  # clean keyframes
-                               do_constraint_clear=False,  # remove constraints from object
-                               do_parents_clear=False,  # don't unparent object
-                               do_object=True,  # bake solid animation
-                               # visual keying bakes in worldspace, but here we want it local since we keep parenting
-                               do_visual_keying=False)
+    baked_action = bakeAction(scene.frame_start,
+                              scene.frame_end,
+                              scene.frame_step,
+                              do_clean=True,  # clean keyframes
+                              do_constraint_clear=False,  # remove constraints from object
+                              do_parents_clear=False,  # don't unparent object
+                              do_object=True,  # bake solid animation
+                              do_pose=True,  # bake skeletal animation
+                              # visual keying bakes in worldspace, but here we want it local since we keep parenting
+                              do_visual_keying=False)
 
     # restore original action
     if original_action:
